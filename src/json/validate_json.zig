b328461c0c3fb90e7fdb54af4c15c04f92cdbda1
Sam Widdowson <samwid@gmail.com>
2025-07-23 23:47:46 +0100

    Finish implementing labelled-switch pattern for validate_json

--- src/json/validate_json.zig
+++ src/json/validate_json.zig
@@ -3,33 +3,41 @@ const std = @import("std");
 const debugPrint = std.debug.print;
 const stdout = std.io.getStdOut().writer();
 
-pub const JsonError = error{ CloseOrNewElementNotFound, KeyNotFound, AssignmentNotFound, ValueNotFound, InvalidValue, InvalidNumber, JsonStartNotFound, UnterminatedString };
+pub const JsonError = error{
+    StartNotFound,
+    KeyNotFound,
+    AssignmentNotFound,
+    ValueNotFound,
+    InvalidValue,
+    InvalidNumber,
+    UnterminatedString,
+    CloseOrNewElementNotFound,
+    UnmatchedSquareBracket,
+    UnmatchedBrace,
+    EndNotFound,
+};
+
 pub const JsonErrorData = struct { offset: usize, character_at_offset: u8 };
 
 const ReadingJsonElement = enum { key, value };
 
-const CharReaderState = enum { start, string_literal, string_literal_backslash, expect_key, expect_value, expect_assignment, expect_new_or_close };
-
-fn isWhitespace(c: u8) bool {
-    if (c == ' ' or c == '\n' or c == '\r') {
-        return true;
-    }
-    return false;
-}
-
-fn isExpectedChar(char: u8, expected: [2]u8) bool {
-    if (expected[0] == char or expected[1] == char) {
-        return true;
-    }
-    return false;
-}
+const CharReaderState = enum {
+    start,
+    string_literal,
+    string_literal_backslash,
+    expect_key,
+    expect_value,
+    expect_assignment,
+    expect_new_or_close,
+    expect_end,
+};
 
 /// Some basic Json validation checking
 /// This will probably be replaced with the load object function
 pub fn validateJson(
     buf: [*:0]const u8,
     error_data: *?JsonErrorData,
-) JsonError!bool {
+) JsonError!void {
     var brace_depth: u16 = 0;
     var array_depth: u16 = 0;
 
@@ -39,6 +47,7 @@ pub fn validateJson(
 
     read: switch (CharReaderState.start) {
         .start => {
+            // debugPrint(".start\n", .{});
             switch (buf[i]) {
                 ' ', '\n', '\r', '\t' => {
                     i += 1;
@@ -49,12 +58,17 @@ pub fn validateJson(
                     continue :read .expect_key;
                 },
                 else => {
-                    error_data.* = JsonErrorData{ .offset = i, .character_at_offset = buf[i] };
-                    return JsonError.UnexpectedJsonStart;
+                    error_data.* = JsonErrorData{
+                        .offset = i,
+                        .character_at_offset = buf[i],
+                    };
+                    return JsonError.StartNotFound;
                 },
             }
         },
         .expect_key => {
+            // debugPrint(".expect_key\n", .{});
+            reading_json_element = .key;
             i += 1;
             switch (buf[i]) {
                 ' ', '\n', '\r', '\t' => {
@@ -64,31 +78,43 @@ pub fn validateJson(
                     continue :read .string_literal;
                 },
                 else => {
-                    error_data.* = JsonErrorData{ .offset = i, .character_at_offset = buf[i] };
-                    return JsonError.ExpectedKeyNotFound;
+                    error_data.* = JsonErrorData{
+                        .offset = i,
+                        .character_at_offset = buf[i],
+                    };
+                    return JsonError.KeyNotFound;
                 },
             }
         },
         .string_literal => {
+            // debugPrint(".string_literal\n", .{});
             i += 1;
             switch (buf[i]) {
                 0, '\n' => {
-                    error_data.* = JsonErrorData{ .offset = i, .character_at_offset = buf[i] };
+                    error_data.* = JsonErrorData{
+                        .offset = i,
+                        .character_at_offset = buf[i],
+                    };
                     return JsonError.UnterminatedString;
                 },
                 '\"' => {
                     if (reading_json_element == .key) {
                         continue :read .expect_assignment;
                     }
+                    continue :read .expect_new_or_close;
                 },
                 else => continue :read .string_literal,
             }
         },
         .string_literal_backslash => {
+            // debugPrint(".string_literal_bs\n", .{});
             i += 1;
             switch (buf[i]) {
                 0 => {
-                    error_data.* = JsonErrorData{ .offset = i, .character_at_offset = buf[i] };
+                    error_data.* = JsonErrorData{
+                        .offset = i,
+                        .character_at_offset = buf[i],
+                    };
                     return JsonError.UnterminatedString;
                 },
                 else => {
@@ -97,22 +123,27 @@ pub fn validateJson(
             }
         },
         .expect_assignment => {
+            // debugPrint(".expect_assignment\n", .{});
             i += 1;
             switch (buf[i]) {
                 ' ', '\n', '\r', '\t' => {
                     continue :read .expect_assignment;
                 },
                 ':' => {
-                    reading_json_element = .value;
                     continue :read .expect_value;
                 },
                 else => {
-                    error_data.* = JsonErrorData{ .offset = i, .character_at_offset = buf[i] };
+                    error_data.* = JsonErrorData{
+                        .offset = i,
+                        .character_at_offset = buf[i],
+                    };
                     return JsonError.AssignmentNotFound;
                 },
             }
         },
         .expect_value => {
+            // debugPrint(".expect_value\n", .{});
+            reading_json_element = .value;
             i += 1;
             switch (buf[i]) {
                 ' ', '\n', '\r', '\t' => {
@@ -123,43 +154,54 @@ pub fn validateJson(
                 },
                 '0'...'9' => {
                     var decimal_point = false;
-                    i += 1;
-                    read_number_literal: switch (buf[i]) {
+                    read_number_literal: switch (buf[i + 1]) {
                         '.' => {
                             if (decimal_point) {
-                                error_data.* = JsonErrorData{ .offset = i, .character_at_offset = buf[i] };
+                                error_data.* = JsonErrorData{
+                                    .offset = i,
+                                    .character_at_offset = buf[i],
+                                };
                                 return JsonError.InvalidNumber;
                             }
                             decimal_point = true;
                             i += 1;
-                            continue :read_number_literal buf[i];
+                            continue :read_number_literal buf[i + 1];
                         },
                         '0'...'9' => {
                             i += 1;
-                            continue :read_number_literal buf[i];
+                            continue :read_number_literal buf[i + 1];
                         },
                         else => continue :read .expect_new_or_close,
                     }
                 },
                 'f' => {
-                    if (!std.mem.eql(buf[i..5], "false")) {
-                        error_data.* = JsonErrorData{ .offset = i, .character_at_offset = buf[i] };
+                    if (!std.mem.eql(u8, buf[i..5], "false")) {
+                        error_data.* = JsonErrorData{
+                            .offset = i,
+                            .character_at_offset = buf[i],
+                        };
                         return JsonError.InvalidValue;
                     }
                     i += 4; // move index to end of word (safe because we confirmed the word aboved)
                     continue :read .expect_new_or_close;
                 },
                 't' => {
-                    if (!std.mem.eql(buf[i..5], "true")) {
-                        error_data.* = JsonErrorData{ .offset = i, .character_at_offset = buf[i] };
+                    if (!std.mem.eql(u8, buf[i..5], "true")) {
+                        error_data.* = JsonErrorData{
+                            .offset = i,
+                            .character_at_offset = buf[i],
+                        };
                         return JsonError.InvalidValue;
                     }
                     i += 3; // move index to end of word (safe because we confirmed the word aboved)
                     continue :read .expect_new_or_close;
                 },
                 'n' => {
-                    if (!std.mem.eql(buf[i..5], "null")) {
-                        error_data.* = JsonErrorData{ .offset = i, .character_at_offset = buf[i] };
+                    if (!std.mem.eql(u8, buf[i..5], "null")) {
+                        error_data.* = JsonErrorData{
+                            .offset = i,
+                            .character_at_offset = buf[i],
+                        };
                         return JsonError.ValueNotFound;
                     }
                     i += 3; // move index to end of word (safe because we confirmed the word aboved)
@@ -169,82 +211,149 @@ pub fn validateJson(
                     array_depth += 1;
                     continue :read .expect_value;
                 },
+                '{' => {
+                    brace_depth += 1;
+                    continue :read .expect_key;
+                },
+                else => {
+                    error_data.* = JsonErrorData{
+                        .offset = i,
+                        .character_at_offset = buf[i],
+                    };
+                    return JsonError.ValueNotFound;
+                },
             }
         },
         .expect_new_or_close => {
+            // debugPrint(".expect_new_etc\n", .{});
             i += 1;
             switch (buf[i]) {
                 ' ', '\n', '\r', '\t' => {
                     continue :read .expect_new_or_close;
                 },
                 ',' => {
-                    std.debug.assert(false); //TODO: implement this
-                    //remember array depth
+                    if (array_depth > 0) {
+                        continue :read .expect_value;
+                    }
+                    continue :read .expect_key;
                 },
                 ']' => {
-                    std.debug.assert(false); //TODO: implement this
-                    //remember array depth
+                    if (array_depth == 0) {
+                        error_data.* = JsonErrorData{
+                            .offset = i,
+                            .character_at_offset = buf[i],
+                        };
+                        return JsonError.UnmatchedSquareBracket;
+                    }
+                    array_depth -= 1;
+                    continue :read .expect_new_or_close;
                 },
                 '}' => {
-                    std.debug.assert(false); //TODO: implement this
-                    //remember array depth
+                    if (brace_depth == 0) {
+                        error_data.* = JsonErrorData{
+                            .offset = i,
+                            .character_at_offset = buf[i],
+                        };
+                        return JsonError.UnmatchedBrace;
+                    }
+
+                    brace_depth -= 1;
+
+                    if (brace_depth > 0) {
+                        continue :read .expect_new_or_close;
+                    }
+                    continue :read .expect_end;
                 },
                 else => {
-                    error_data.* = JsonErrorData{ .offset = i, .character_at_offset = buf[i] };
+                    error_data.* = JsonErrorData{
+                        .offset = i,
+                        .character_at_offset = buf[i],
+                    };
                     return JsonError.CloseOrNewElementNotFound;
                 },
             }
         },
+        .expect_end => {
+            debugPrint(".expect_end\n", .{});
+            i += 1;
+            switch (buf[i]) {
+                ' ', '\n', '\r', '\t' => {
+                    continue :read .expect_end;
+                },
+                0 => {
+                    // return without error: we found the end where we expected it to be!
+                    return;
+                },
+                else => {
+                    error_data.* = JsonErrorData{
+                        .offset = i,
+                        .character_at_offset = buf[i],
+                    };
+                    return JsonError.EndNotFound;
+                },
+            }
+        },
     }
+    debugPrint("Ooooops! Dropped out at index {d}\n", .{i});
+    unreachable;
 }
 
 pub fn outputJsonError(error_data: JsonErrorData) !void {
     try stdout.print("error offset: {d}\n", .{error_data.offset});
     try stdout.print("error character at  offset: {c}\n", .{error_data.character_at_offset});
-    try stdout.print("error expected characters: {s}\n", .{error_data.expected_characters});
 }
 
 /// TESTS
 const expect = std.testing.expect;
+const assert = std.debug.assert;
+
 test "validateJson returns true for valid json" {
     var error_data: ?JsonErrorData = null;
     const valid_json = @embedFile("tragedian.json");
 
-    const is_valid = try validateJson(valid_json, &error_data);
-    try expect(is_valid);
+    validateJson(valid_json, &error_data) catch |err| {
+        debugPrint("Unexpected error validation JSON: {}", .{err});
+        assert(false);
+    };
+
     try expect(error_data == null);
 }
 
-test "validateJson returns UnexpectedChar and offset correctly" {
+test "validateJson returns StartNotFound when first non-whitespace character is not `{`" {
+    var error_data: ?JsonErrorData = null;
+    const unexpected_char_json = " \r\n\t\"mask\":\"white\"*\"body\":\"black\"}";
+    const err = validateJson(unexpected_char_json, &error_data);
+    try expect(err == JsonError.StartNotFound);
+    try expect(error_data.?.offset == 4);
+    try expect(error_data.?.character_at_offset == '"');
+}
+
+test "validateJson returns CloseOrNewElementNotFound when values are not followed by `,` or `}`" {
     var error_data: ?JsonErrorData = null;
     const unexpected_char_json = "{\"mask\":\"white\"*\"body\":\"black\"}";
-
-    const is_valid = validateJson(unexpected_char_json, &error_data);
-    try expect(is_valid == JsonError.UnexpectedChar);
+    const err = validateJson(unexpected_char_json, &error_data);
+    try expect(err == JsonError.CloseOrNewElementNotFound);
     try expect(error_data.?.offset == 15);
     try expect(error_data.?.character_at_offset == '*');
-    try expect(error_data.?.expected_characters[0] == ',');
-    try expect(error_data.?.expected_characters[1] == '}');
 }
 
-test "validateJson returns UnmatchedOpeningBrace correctly" {
+test "validateJson returns AssignmentNotFound when keys are not followed by `,`" {
     var error_data: ?JsonErrorData = null;
-    const unterminated_string_json = "{\"mask\":\"white\",\"body\":\"black\"";
+    const unexpected_char_json = "{\"mask\"\"white\",\"body\":\"black\"}";
 
-    const is_valid = validateJson(unterminated_string_json, &error_data);
+    const err = validateJson(unexpected_char_json, &error_data);
 
-    // outputJsonError(error_data.?);
-
-    try expect(is_valid == JsonError.UnmatchedOpeningBrace);
+    try expect(err == JsonError.AssignmentNotFound);
+    try expect(error_data.?.offset == 7);
+    try expect(error_data.?.character_at_offset == '"');
 }
 
-test "validateJson returns UnmatchedClosingBrace correctly" {
-    var error_data: ?JsonErrorData = null;
-    const unterminated_string_json = "{\"mask\":\"white\",\"body\":\"black\"}}";
-
-    const is_valid = validateJson(unterminated_string_json, &error_data);
-
-    // outputJsonError(error_data.?);
-
-    try expect(is_valid == JsonError.UnmatchedClosingBrace);
-}
+// TODO: errors to test:
+// KeyNotFound,
+// ValueNotFound,
+// InvalidValue,
+// InvalidNumber,
+// UnterminatedString,
+// UnmatchedSquareBracket,
+// UnmatchedBrace,
+// EndNotFound,
